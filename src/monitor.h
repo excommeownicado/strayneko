67d63d0b133f4ebc6d69b0a2ace0057697a175f7
excommeownicado <93788995+excommeownicado@users.noreply.github.com>
2026-08-10 00:36:01 +0500

    refactor: move monitor types into monitor header

--- src/monitor.h
+++ src/monitor.h
@@ -3,6 +3,20 @@
 
 #include "geometry.h"
 
+typedef struct {
+    int x;
+    int y;
+    int width;
+    int height;
+} MonitorRect;
+
+typedef struct {
+    int min_x;
+    int min_y;
+    int max_x;
+    int max_y;
+} MonitorBounds;
+
 int RectOnMonitor(
     int x,
     int y,
