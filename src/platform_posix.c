dea0693b747fd1125e594d9be8b122d4e7d98988
excommeownicado <93788995+excommeownicado@users.noreply.github.com>
2026-08-10 01:03:41 +0500

    refactor: add POSIX platform timing backend

---
+++ src/platform_posix.c
@@ -0,0 +1,32 @@
+#include "platform.h"
+
+#include <errno.h>
+#include <time.h>
+
+uint64_t
+PlatformGetTimeMs(void)
+{
+    struct timespec ts;
+
+    if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0) {
+        return 0;
+    }
+
+    return (uint64_t)ts.tv_sec * 1000ULL +
+           (uint64_t)ts.tv_nsec / 1000000ULL;
+}
+
+void
+PlatformSleepUs(uint64_t microseconds)
+{
+    struct timespec request;
+
+    request.tv_sec = (time_t)(microseconds / 1000000ULL);
+    request.tv_nsec = (long)((microseconds % 1000000ULL) * 1000ULL);
+
+    while (nanosleep(&request, &request) != 0) {
+        if (errno != EINTR) {
+            break;
+        }
+    }
+}
