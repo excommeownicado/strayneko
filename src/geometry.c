5f9476d66a22e997649feece89ff4fdda7e3387e
excommeownicado <93788995+excommeownicado@users.noreply.github.com>
2026-08-10 00:31:41 +0500

    refactor: implement geometry primitives

--- src/geometry.c
+++ src/geometry.c
@@ -1,5 +1,79 @@
 #include "strayneko.h"
 
+bool
+PointInRect(Point point, Rect rect)
+{
+    int64_t right = (int64_t)rect.x + rect.width;
+    int64_t bottom = (int64_t)rect.y + rect.height;
+
+    return point.x >= rect.x &&
+           point.y >= rect.y &&
+           (int64_t)point.x < right &&
+           (int64_t)point.y < bottom;
+}
+
+bool
+RectIntersectsRect(Rect a, Rect b)
+{
+    int64_t a_right = (int64_t)a.x + a.width;
+    int64_t a_bottom = (int64_t)a.y + a.height;
+    int64_t b_right = (int64_t)b.x + b.width;
+    int64_t b_bottom = (int64_t)b.y + b.height;
+
+    return a.x < b_right &&
+           a_right > b.x &&
+           a.y < b_bottom &&
+           a_bottom > b.y;
+}
+
+bool
+RectInsideRect(Rect object, Rect container)
+{
+    int64_t object_right = (int64_t)object.x + object.width;
+    int64_t object_bottom = (int64_t)object.y + object.height;
+    int64_t container_right = (int64_t)container.x + container.width;
+    int64_t container_bottom = (int64_t)container.y + container.height;
+
+    return object.x >= container.x &&
+           object.y >= container.y &&
+           object_right <= container_right &&
+           object_bottom <= container_bottom;
+}
+
+Rect
+ClampRectToRect(Rect object, Rect container)
+{
+    Rect result = object;
+
+    if (result.width >= container.width) {
+        result.x = container.x;
+    } else {
+        if (result.x < container.x) {
+            result.x = container.x;
+        }
+
+        if ((int64_t)result.x + result.width >
+            (int64_t)container.x + container.width) {
+            result.x = container.x + container.width - result.width;
+        }
+    }
+
+    if (result.height >= container.height) {
+        result.y = container.y;
+    } else {
+        if (result.y < container.y) {
+            result.y = container.y;
+        }
+
+        if ((int64_t)result.y + result.height >
+            (int64_t)container.y + container.height) {
+            result.y = container.y + container.height - result.height;
+        }
+    }
+
+    return result;
+}
+
 MonitorRect
 GetMonitorRect(int monitor)
 {
@@ -22,52 +96,51 @@ GetMonitorRect(int monitor)
 int
 RectOnMonitor(int x, int y, int w, int h)
 {
-    int i;
-
     if (w < 0 || h < 0) {
         return 0;
     }
 
-    int64_t right = (int64_t)x + w;
-    int64_t bottom = (int64_t)y + h;
+    Rect object = {
+        .x = x,
+        .y = y,
+        .width = w,
+        .height = h
+    };
 
     if (MonitorCount <= 0) {
-        return x >= 0 &&
-               y >= 0 &&
-               right <= WindowWidth &&
-               bottom <= WindowHeight;
+        Rect root = {
+            .x = 0,
+            .y = 0,
+            .width = (int)WindowWidth,
+            .height = (int)WindowHeight
+        };
+
+        return RectInsideRect(object, root);
     }
 
     if (Config.restrict_monitor >= 0 &&
         Config.restrict_monitor < MonitorCount) {
+        MonitorRect monitor = GetMonitorRect(Config.restrict_monitor);
+        Rect container = {
+            .x = monitor.x,
+            .y = monitor.y,
+            .width = monitor.width,
+            .height = monitor.height
+        };
 
-        MonitorRect rect = GetMonitorRect(Config.restrict_monitor);
-
-        int64_t monitor_right =
-            (int64_t)rect.x + rect.width;
-
-        int64_t monitor_bottom =
-            (int64_t)rect.y + rect.height;
-
-        return x >= rect.x &&
-               y >= rect.y &&
-               right <= monitor_right &&
-               bottom <= monitor_bottom;
+        return RectInsideRect(object, container);
     }
 
-    for (i = 0; i < MonitorCount; i++) {
-        MonitorRect rect = GetMonitorRect(i);
+    for (int i = 0; i < MonitorCount; i++) {
+        MonitorRect monitor = GetMonitorRect(i);
+        Rect container = {
+            .x = monitor.x,
+            .y = monitor.y,
+            .width = monitor.width,
+            .height = monitor.height
+        };
 
-        int64_t monitor_right =
-            (int64_t)rect.x + rect.width;
-
-        int64_t monitor_bottom =
-            (int64_t)rect.y + rect.height;
-
-        if (x >= rect.x &&
-            y >= rect.y &&
-            right <= monitor_right &&
-            bottom <= monitor_bottom) {
+        if (RectInsideRect(object, container)) {
             return 1;
         }
     }
@@ -78,16 +151,21 @@ RectOnMonitor(int x, int y, int w, int h)
 int
 FindMonitorFor(int x, int y)
 {
+    Point point = {
+        .x = x,
+        .y = y
+    };
+
     for (int i = 0; i < MonitorCount; i++) {
-        MonitorRect rect = GetMonitorRect(i);
+        MonitorRect monitor = GetMonitorRect(i);
+        Rect rect = {
+            .x = monitor.x,
+            .y = monitor.y,
+            .width = monitor.width,
+            .height = monitor.height
+        };
 
-        int64_t right = (int64_t)rect.x + rect.width;
-        int64_t bottom = (int64_t)rect.y + rect.height;
-
-        if (x >= rect.x &&
-            y >= rect.y &&
-            (int64_t)x < right &&
-            (int64_t)y < bottom) {
+        if (PointInRect(point, rect)) {
             return i;
         }
     }
@@ -99,7 +177,7 @@ MonitorBounds
 GetMonitorBounds(int monitor)
 {
     MonitorBounds bounds = {0};
-    
+
     if (!Monitors ||
         monitor < 0 ||
         monitor >= MonitorCount) {
@@ -110,14 +188,14 @@ GetMonitorBounds(int monitor)
 
     bounds.min_x = rect.x;
     bounds.min_y = rect.y;
-    
+
     int64_t max_x = (int64_t)rect.x +
-                rect.width -
-                BITMAP_WIDTH;
+                    rect.width -
+                    BITMAP_WIDTH;
 
     int64_t max_y = (int64_t)rect.y +
-                rect.height -
-                BITMAP_HEIGHT;
+                    rect.height -
+                    BITMAP_HEIGHT;
 
     bounds.max_x = max_x > rect.x
         ? (int)max_x
