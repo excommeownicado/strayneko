e2547210781b83cec52ddf3235c9f5b8f6ee6d4d
excommeownicado <93788995+excommeownicado@users.noreply.github.com>
2026-08-10 00:36:52 +0500

    refactor: track explicitly configured numeric options

--- src/config.h
+++ src/config.h
@@ -1,12 +1,17 @@
 #ifndef STRAYNEKO_CONFIG_H
 #define STRAYNEKO_CONFIG_H
 
+#include <stdbool.h>
+
 typedef struct {
     char *foreground;
     char *background;
 
     long interval_time;
+    bool interval_time_set;
+
     double speed;
+    bool speed_set;
 
     int no_shape;
 
