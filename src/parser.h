51a8bc0658aa23d93bda1325e3a0d2efcd6732d7
excommeownicado <93788995+excommeownicado@users.noreply.github.com>
2026-08-10 00:32:41 +0500

    refactor: add boolean option parser

--- src/parser.h
+++ src/parser.h
@@ -1,6 +1,8 @@
 #ifndef STRAYNEKO_PARSER_H
 #define STRAYNEKO_PARSER_H
 
+#include <stdbool.h>
+
 int ParseLongOption(
     const char *value,
     long minimum,
@@ -11,6 +13,11 @@ int ParseDoubleOption(
     const char *value,
     double minimum,
     double *result
+);
+
+bool ParseBoolOption(
+    const char *value,
+    bool *result
 );
 
 #endif /* STRAYNEKO_PARSER_H */
