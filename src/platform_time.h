fbd93d798120991ee2198709fcd7dadefe1d5b10
excommeownicado <93788995+excommeownicado@users.noreply.github.com>
2026-08-10 22:35:18 +0500

    refactor: make platform timing header a compatibility wrapper

--- src/platform_time.h
+++ src/platform_time.h
@@ -1,9 +1,7 @@
 #ifndef STRAYNEKO_PLATFORM_TIME_H
 #define STRAYNEKO_PLATFORM_TIME_H
 
-#include <stdint.h>
+/* Compatibility header. New code should include platform.h directly. */
+#include "platform.h"
 
-uint64_t PlatformGetTimeMs(void);
-void PlatformSleepUs(uint64_t microseconds);
-
-#endif
+#endif /* STRAYNEKO_PLATFORM_TIME_H */
