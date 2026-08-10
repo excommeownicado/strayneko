3b7ffa26dc7ef3d6223e4394f2ba770f92843c28
excommeownicado <93788995+excommeownicado@users.noreply.github.com>
2026-08-10 15:11:08 +0500

    refactor: expose X11 window creation API

--- src/x11_window.h
+++ src/x11_window.h
@@ -3,6 +3,21 @@
 
 #include <X11/Xlib.h>
 
+Window X11WindowCreate(
+    Display *display,
+    Window parent,
+    int x,
+    int y,
+    unsigned int width,
+    unsigned int height,
+    unsigned int border_width,
+    int depth,
+    unsigned int class,
+    Visual *visual,
+    unsigned long valuemask,
+    XSetWindowAttributes *attributes
+);
+
 void X11WindowMove(Display *display, Window window, int x, int y);
 void X11WindowRaise(Display *display, Window window);
 void X11WindowMap(Display *display, Window window);
