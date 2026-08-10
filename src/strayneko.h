586ff1fcd532ac9ce14461ad6149566597bef619
excommeownicado <93788995+excommeownicado@users.noreply.github.com>
2026-08-10 15:14:40 +0500

    refactor: route X11 event polling through platform layer

--- src/strayneko.h
+++ src/strayneko.h
@@ -5,6 +5,7 @@
 #include "monitor.h"
 #include "config.h"
 #include "x11_window_compat.h"
+#include "x11_event_compat.h"
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
@@ -25,14 +26,11 @@
 
 #define BITMAP_WIDTH 32
 #define BITMAP_HEIGHT 32
-
 #define AVAIL_KEYBUF 255
 #define MAX_TICK 9999
 #define DEFAULT_RAISE_WAIT 16
-
 #define DEFAULT_FOREGROUND "black"
 #define DEFAULT_BACKGROUND "white"
-
 #define NEKO_STOP 0
 #define NEKO_JARE 1
 #define NEKO_KAKI 2
@@ -51,111 +49,53 @@
 #define NEKO_D_TOGI 15
 #define NEKO_L_TOGI 16
 #define NEKO_R_TOGI 17
-
 #define NEKO_STATE_COUNT 18
-
 #define NEKO_STOP_TIME 4
 #define NEKO_JARE_TIME 10
 #define NEKO_KAKI_TIME 4
 #define NEKO_AKUBI_TIME 6
 #define NEKO_AWAKE_TIME 3
 #define NEKO_TOGI_TIME 10
-
 #define PI_PER8 ((double)3.1415926535/(double)8)
 #define MAXDISPLAYNAME (64 + 5)
 #define NOTDEFINED (-1)
-
 #define IsTrue(str) (strcmp((str), "true") == 0)
 
 typedef enum {
-    SPRITE_MATI2,
-    SPRITE_JARE2,
-    SPRITE_KAKI1,
-    SPRITE_KAKI2,
-    SPRITE_MATI3,
-    SPRITE_SLEEP1,
-    SPRITE_SLEEP2,
-    SPRITE_AWAKE,
-
-    SPRITE_UP1,
-    SPRITE_UP2,
-    SPRITE_DOWN1,
-    SPRITE_DOWN2,
-    SPRITE_LEFT1,
-    SPRITE_LEFT2,
-    SPRITE_RIGHT1,
-    SPRITE_RIGHT2,
-
-    SPRITE_UPLEFT1,
-    SPRITE_UPLEFT2,
-    SPRITE_UPRIGHT1,
-    SPRITE_UPRIGHT2,
-    SPRITE_DOWNLEFT1,
-    SPRITE_DOWNLEFT2,
-    SPRITE_DOWNRIGHT1,
-    SPRITE_DOWNRIGHT2,
-
-    SPRITE_UPTOGI1,
-    SPRITE_UPTOGI2,
-    SPRITE_DOWNTOGI1,
-    SPRITE_DOWNTOGI2,
-    SPRITE_LEFTTOGI1,
-    SPRITE_LEFTTOGI2,
-    SPRITE_RIGHTTOGI1,
-    SPRITE_RIGHTTOGI2,
-
+    SPRITE_MATI2, SPRITE_JARE2, SPRITE_KAKI1, SPRITE_KAKI2,
+    SPRITE_MATI3, SPRITE_SLEEP1, SPRITE_SLEEP2, SPRITE_AWAKE,
+    SPRITE_UP1, SPRITE_UP2, SPRITE_DOWN1, SPRITE_DOWN2,
+    SPRITE_LEFT1, SPRITE_LEFT2, SPRITE_RIGHT1, SPRITE_RIGHT2,
+    SPRITE_UPLEFT1, SPRITE_UPLEFT2, SPRITE_UPRIGHT1, SPRITE_UPRIGHT2,
+    SPRITE_DOWNLEFT1, SPRITE_DOWNLEFT2, SPRITE_DOWNRIGHT1, SPRITE_DOWNRIGHT2,
+    SPRITE_UPTOGI1, SPRITE_UPTOGI2, SPRITE_DOWNTOGI1, SPRITE_DOWNTOGI2,
+    SPRITE_LEFTTOGI1, SPRITE_LEFTTOGI2, SPRITE_RIGHTTOGI1, SPRITE_RIGHTTOGI2,
     SPRITE_COUNT
 } SpriteID;
 
-typedef struct {
-    GC gc;
-    Pixmap pixmap;
-    Pixmap mask;
-} Sprite;
+typedef struct { GC gc; Pixmap pixmap; Pixmap mask; } Sprite;
+typedef struct { const unsigned char *bits; const unsigned char *mask_bits; } SpriteData;
 
 typedef struct {
-    const unsigned char *bits;
-    const unsigned char *mask_bits;
-} SpriteData;
-
-typedef struct {
-    int x;
-    int y;
-    int target_x;
-    int target_y;
-
-    int move_dx;
-    int move_dy;
-
-    int last_x;
-    int last_y;
+    int x, y, target_x, target_y;
+    int move_dx, move_dy;
+    int last_x, last_y;
     GC last_gc;
     SpriteID last_sprite;
-
-    int tick_count;
-    int state_count;
-    int state;
-
+    int tick_count, state_count, state;
     int waiting;
     time_t next_move_time;
-
     int zoomies;
     time_t zoomies_end_time;
-
     int raise_window_delay;
 } NekoData;
 
 typedef struct {
-    int x;
-    int y;
-
+    int x, y;
     int going_to_bed;
     Bool enabled;
-
     int dragging;
-    int drag_offset_x;
-    int drag_offset_y;
-
+    int drag_offset_x, drag_offset_y;
     GC gc;
     Pixmap pixmap;
     Pixmap mask;
@@ -164,43 +104,32 @@ typedef struct {
 extern NekoData Neko;
 extern BedData Bed;
 extern ConfigData Config;
-
 extern const char *ClassName;
 extern char *ProgramName;
-
 extern Display *theDisplay;
 extern int theScreen;
 extern unsigned int theDepth;
 extern Window theRoot;
 extern Window theWindow;
 extern Window BedWindow;
-
 extern unsigned int WindowWidth;
 extern unsigned int WindowHeight;
-
 extern Sprite Sprites[SPRITE_COUNT];
-
 extern XColor theForegroundColor;
 extern XColor theBackgroundColor;
-
 extern int Synchronous;
 extern volatile sig_atomic_t TerminationRequested;
-
 extern Bool DontMapped;
-
 extern XRRMonitorInfo *Monitors;
 extern int MonitorCount;
-
 extern int ForceTargetFlag;
 extern int ForceTargetX;
 extern int ForceTargetY;
-
 #ifdef ENABLE_DEBUG
 extern int DebugMode;
 #else
 #define DebugMode 0
 #endif
-
 extern double SinPiPer8Times3;
 extern double SinPiPer8;
 
