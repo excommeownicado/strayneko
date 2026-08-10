0c4a5c466d88d0b6458dc707c113f4f469ccfc2b
excommeownicado <93788995+excommeownicado@users.noreply.github.com>
2026-08-10 23:56:03 +0500

    Hide X11 event types behind platform API

--- src/x11_platform_events.h
+++ src/x11_platform_events.h
@@ -1,13 +1,9 @@
 #ifndef STRAYNEKO_X11_PLATFORM_EVENTS_H
 #define STRAYNEKO_X11_PLATFORM_EVENTS_H
 
-#include "platform_event.h"
+#include "platform.h"
 
-#include <X11/Xlib.h>
-
-/* Translate one pending X11 event into a platform-neutral event.
- * Returns false when there is no event to consume. */
-bool X11PollPlatformEvent(Display *display, Window neko_window,
-                           Window bed_window, PlatformEvent *event);
+/* X11 backend implementation of the platform-neutral event API. */
+bool PlatformPollEvent(PlatformEvent *event);
 
 #endif
